66c0a8635718fde3d7b8328bc33b62ed13bb6fd8
Vanness Lao <vannesslao01@gmail.com>
2024-12-07 01:32:25 +0800

    added answer key pop up

--- result.js
+++ result.js
@@ -16,3 +16,15 @@ const scoreDisplay = document.getElementById('score-display');
 scoreDisplay.textContent = `${score}/${totalQuestions}`;
 
 localStorage.setItem('score', 0);
+
+function openShowAnswersPopUp() {
+  document.getElementById('show-answers-pop-up').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+function closePopup() {
+  document.getElementById('show-answers-pop-up').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
+  document.body.style.overflow = '';
+}
